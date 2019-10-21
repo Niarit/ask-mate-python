2fae6a306102b75698b5573049ae59c8dfd420b4
Nia <zsofi.deak.nia@gmail.com>
2019-10-21 13:26:20 +0200

    Implement route in senrver

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 
-# import data_handler
+import data_handler
 
 app = Flask(__name__)
 
@@ -8,7 +8,8 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/list')
 def route_list():
-    pass
+    questions = data_handler.get_all_data(break_lines=True)
+    return render_template('list.html', questions=questions)
 
 
 @app.route('/add')
