2db42c7d5ffcfbb5343594d347c8a53e5e3d3362
Zoltan Jakubecz <jakuzo@gmail.com>
2019-11-05 10:12:35 +0100

    Add Database connection

--- connection.py
+++ connection.py
@@ -1,41 +1,44 @@
-import csv
-import time
+import os
+import psycopg2
+import psycopg2.extras
 
 
-def get_all_data(csv_file, break_lines=True):
-    with open(csv_file, encoding='utf-8') as file:
-        reader = csv.DictReader(file)
-        all_data = list(reader)
-        for row in all_data:
-            if break_lines:
-                row['message'] = convert_enter_to_br(row['message'])
-            for key, val in row.items():
-                if is_number(val):
-                    row[key] = int(val)
-        return all_data
+def get_connection_string():
+    user_name = os.environ.get('PSQL_USER_NAME')
+    password = os.environ.get('PSQL_PASSWORD')
+    host = os.environ.get('PSQL_HOST')
+    database_name = os.environ.get('PSQL_DB_NAME')
+
+    env_variables_defined = user_name and password and host and database_name
+    if env_variables_defined:
+        return 'postgresql://{user_name}:{password}@{host}/{database_name}'.format(
+            user_name=user_name,
+            password=password,
+            host=host,
+            database_name=database_name
+        )
+    else:
+        raise KeyError('Some necessary environment variable(s) are not defined')
 
 
-def is_number(s):
+def open_database():
     try:
-        int(s)
-        return True
-    except ValueError:
-        pass
-    return False
+        connection_string = get_connection_string()
+        connection = psycopg2.connect(connection_string)
+        connection.autocommit = True
+    except psycopg2.DatabaseError as exception:
+        print('Database connection problem')
+        raise exception
+    return connection
 
 
-def convert_enter_to_br(original_string):
-    return '<br>'.join(original_string.split('\n'))
+def connection_handler(function):
+    def wrapper(*args, **kwargs):
+        connection = open_database()
+        dict_cur = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        ret_value = function(dict_cur, *args, **kwargs)
+        dict_cur.close()
+        connection.close()
+        return ret_value
 
-
-def get_submission_time():
-    return int(time.time())
-
-
-def creat_new_id(csv_file):
-    existing_data = get_all_data(csv_file)
-    if len(existing_data) == 0:
-        return'1'
-    return str(int(existing_data[-1]['id'])+1)
-
-
+    return wrapper
