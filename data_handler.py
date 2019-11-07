Niarit/ask-mate-python

commit cd2ddf134df7a2ae78053a4f7b1d801a5dab78c3

Implement edit answer endpoint

data_handler.py
@@ -50,6 +50,13 @@ def get_one_question(question_id):
     return question_data
 
 
+def get_answer_with_its_question(answer_id):
+    answer_data = DAL.answers.select_one(answer_id)
+    question_data = DAL.questions.select_one(answer_data['question_id'])
+    answer_data['question'] = question_data
+    return answer_data
+
+
 def get_answers_for_a_question(question_id):
     answers = DAL.answers.get_answers_for_a_question(question_id)
     return answers
@@ -113,6 +120,13 @@ def delete_answer(answer_id, app):
     __delete_image(answer, app)
     DAL.answers.delete(answer)
     return question_id
+
+
+def edit_answer(request):
+    answer = dict(request)
+    DAL.answers.update(answer)
+    answer = DAL.answers.select_one(answer['id'])
+    return answer['question_id']
 
 
 def __upload_file_if_any(form_request, item, send_from_directory, app):

DAL/answers.py
@@ -54,15 +54,11 @@ def add_new(cursor, data):
 def update(cursor, data):
     cursor.execute("""
                     UPDATE answer
-                    SET vote_number = %(vote_number)s,
-                        message = %(message)s,
-                        image = %(image)s
+                    SET message = %(message)s
                     WHERE id = %(id)s;
                     """,
                    {
-                       'vote_number': data['vote_number'],
                        'message': data['message'],
-                       'image': data['image'],
                        'id': data['id']
                    })
 

server.py
@@ -96,6 +96,15 @@ def search():
     return render_template('question/display_all.html', questions=result)
 
 
+@app.route('/answer/<int:answer_id>/edit', methods=['GET', 'POST'])
+def edit_answer(answer_id):
+    if request.method == 'POST':
+        question_id = data_handler.edit_answer(request.form)
+        return redirect(url_for('show_answers', question_id=question_id))
+    answer = data_handler.get_answer_with_its_question(answer_id)
+    return render_template('answer/edit.html', answer=answer)
+
+
 if __name__ == '__main__':
     app.run(
         host='0.0.0.0',

templates/answer/edit.html
@@ -1,0 +1,25 @@
+{% extends 'base.html' %}
+
+{% block title %}
+    Ask Mate! | Edit
+{% endblock %}
+
+{% block content %}
+    <div class="jumbotron">
+        <h1>{{ answer['question']['title'] }}</h1>
+
+        <p>{{ answer['question']['message'] | safe }}</p>
+
+        <form action="/answer/{{ answer['id'] }}/edit" method="POST" enctype="multipart/form-data">
+            <input type="hidden" name="id" value="{{ answer['id'] }}">
+
+            <div class="form-group">
+                <label for="message">Answer:</label><br>
+                <textarea class="form-control" name="message" id="message" cols="40" rows="4" required>{{ answer['message'].replace('<br>', '\n') }}</textarea>
+            </div>
+            <button type="submit" class="btn btn-primary mb-3">Update answer</button>
+        </form>
+
+        <a class="btn btn-info" href="{{ url_for('show_questions') }}">Back to Main menu</a>
+    </div>
+{% endblock %}

templates/question/display_one.html
@@ -48,6 +48,9 @@
                             <td class="f-right">
                                 <a class="btn btn-danger" href="/answer/{{ answer['id'] }}/delete?question_id={{ question['id'] }}">Delete answer</a>
                             </td>
+                            <td class="f-right">
+                                <a class="btn btn-warning" href="/answer/{{ answer['id'] }}/edit">Edit answer</a>
+                            </td>
                         </tr>
                     {% endfor %}
                 {% else %}

commit 09d3804edccd9d02ce679038e4f28e069cf4e7c0

Fix editing answer throws error
I introduced a bug in cd2ddf134df7a2ae78053a4f7b1d801a5dab78c3
which was fixed in 994b31c6ff86fe4695c87990c947a6d1b7c8b5f6
but the fix broke the new feature.

data_handler.py
@@ -124,9 +124,10 @@ def delete_answer(answer_id, app):
 
 
 def edit_answer(request):
-    answer = dict(request)
-    DAL.answers.update(answer)
-    answer = DAL.answers.select_one(answer['id'])
+    edited_answer = dict(request)
+    answer = DAL.answers.select_one(edited_answer['id'])
+    edited_answer['vote_number'] = answer['vote_number']
+    DAL.answers.update(edited_answer)
     return answer['question_id']
 
 