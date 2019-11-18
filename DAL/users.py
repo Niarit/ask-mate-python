2cd41943277055e97eff343dfdcb708084463b25
nia <zsofi.deak.nia@gmail.com>
2019-11-18 13:28:05 +0100

    Implement show all users page

--- DAL/users.py
+++ DAL/users.py
@@ -7,6 +7,14 @@ def add_user(cursor, data):
                     INSERT INTO users (user_name, pw)
                     VALUES (%(user_name)s, %(pw)s)""",
                    {
-                       'user_name' : data['user_name']
+                       'user_name' : data['user_name'],
                        'pw' : data['password']
                    })
+
+
+@connection.connection_handler
+def get_all_users(cursor):
+    cursor.execute("""
+                    SELECT id, user_name FROM users""")
+    all_users = cursor.fetchall()
+    return all_users
