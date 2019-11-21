a9064da37a025db91aa2e3328ff2884847a65110
Tom PoLáKoSz <polakosz@freemail.hu>
2019-11-21 15:41:33 +0100

    Fix key error when HTML name attribute not match in validation.py

--- validate.py
+++ validate.py
@@ -6,13 +6,13 @@ Module to validate user input.
 def as_question(question):
     error_bag = []
 
-    if question['title']:
+    if 'title' in question:
         if len(question['title']) < 6:
             error_bag.append('The title of the question has to be at least 6 characters long!')
     else:
         error_bag.append('Please specify the question title!')
 
-    if question['message']:
+    if 'message' in question:
         if len(question['message']) < 10:
             error_bag.append('The content of the question has to be at least 10 characters long!')
     else:
