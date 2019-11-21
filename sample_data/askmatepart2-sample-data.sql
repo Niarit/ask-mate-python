61790802cbba6e7a45f87c82f1c6a9b3a62710a0
Tom PoLáKoSz <polakosz@freemail.hu>
2019-11-21 11:15:23 +0100

    Fix Users table ID next value for insert

--- sample_data/askmatepart2-sample-data.sql
+++ sample_data/askmatepart2-sample-data.sql
@@ -137,6 +137,8 @@ INSERT INTO users VALUES (6, 'Surcingle','$2b$12$NIB/JMT4b5v8I4wN4gn.lec4jonisMh
 INSERT INTO users VALUES (7, 'Syncline','$2b$12$NIB/JMT4b5v8I4wN4gn.lec4jonisMhwAnvb//DhykEkIFRXFU.PC', 0, '2019-12-01 04:50:00');
 INSERT INTO users VALUES (8, 'Trapezoid','$2b$12$NIB/JMT4b5v8I4wN4gn.lec4jonisMhwAnvb//DhykEkIFRXFU.PC', 0, '2017-01-05 06:32:00');
 
+SELECT pg_catalog.setval('users_id_seq', 8, true);
+
 INSERT INTO question VALUES (0, '2017-04-28 08:29:00', 29, 7, 'How to make lists in Python?', 'I am totally new to this, any hints?', NULL, 1);
 INSERT INTO question VALUES (1, '2017-04-29 09:19:00', 15, 9, 'Wordpress loading multiple jQuery Versions', 'I developed a plugin that uses the jquery booklet plugin (http://builtbywill.com/booklet/#/) this plugin binds a function to $ so I cann call $(".myBook").booklet();
 
